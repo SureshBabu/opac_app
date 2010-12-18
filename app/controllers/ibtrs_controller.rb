a4dcc71a5d36971c6b3e177c142e853de9a45389
akhilesh <kataria.subs@gmail.com>
2010-12-19 02:51:37 +0530

    panicgit status - just a few bugs

--- app/controllers/ibtrs_controller.rb
+++ app/controllers/ibtrs_controller.rb
@@ -1,12 +1,12 @@
 class IbtrsController < ApplicationController
   def index
-    @ibts = Ibtr.paginate :page => params[:page], :order => 'created_at DESC'
+    @ibtrs = Ibtr.paginate :page => params[:page], :order => 'created_at DESC'
   end
   
   def update
     @ibtr = Ibtr.find(params[:id])
     @ibtr.processEvent(params[:ibtr][:event])
-    @ibtr.update_attributes(:respondent_id => params[:ibtr][:respondent_id], :state => @ibtr.current_state, :title_id => params[:ibtr][:title_id])
+    @ibtr.update_attributes(:respondent_id => params[:ibtr][:respondent_id], :state => @ibtr.current_state)
     flash[:notice] = "Successfully #{@ibtr.state} " << (params[:ibtr][:event].eql?("assign") ? "to #{@ibtr.respondent_id}!" : "!")
   rescue Transitions::InvalidTransition
     flash[:error] = "Cannot #{params[:ibtr][:event]} a request that is #{@ibtr.state}."
