af3d89b8da63180ab105fa6662a7b48090a3ce15
akhilesh <kataria.subs@gmail.com>
2010-12-21 16:11:44 +0530

    renamed comment to comments

--- app/controllers/ibtrs_controller.rb
+++ app/controllers/ibtrs_controller.rb
@@ -8,7 +8,7 @@ class IbtrsController < ApplicationController
     @ibtr.processEvent(params[:ibtr][:event])
     @ibtr.update_attributes(:respondent_id => params[:ibtr][:respondent_id], 
                             :reason_id => params[:ibtr][:reason_id], 
-                            :comment => params[:ibtr][:comment], 
+                            :comments => params[:ibtr][:comments], 
                             :state => @ibtr.current_state)
     flash[:notice] = "Successfully #{@ibtr.state} " << (params[:ibtr][:event].eql?("assign") ? "to #{@ibtr.respondent_id}!" : "!")
   rescue Transitions::InvalidTransition
